c3b6866bdca7d3e1243fbbc648b1cb187d0a1934
Colton Smart <csmart@URE-0172.local>
2025-02-24 22:31:46 -0700

    fixing linting issues.

--- src/components/GameLayout.tsx
+++ src/components/GameLayout.tsx
@@ -1,5 +1,3 @@
-import NavBar from './NavBar'
-
 interface GameLayoutProps {
     children: React.ReactNode
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -102,7 +102,7 @@ export default function NavBar() {
                         <Menu.Items className="absolute left-0 z-10 mt-2 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           {games.map((game) => (
                             <Menu.Item key={game.name}>
-                              {({ active }) => (
+                              {() => (
                                 <Link
                                   href={game.href}
                                   className={classNames(
